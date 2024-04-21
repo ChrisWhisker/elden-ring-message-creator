dbc254c739a733dd65dcba786954f2116c7f3a9f
Chris Worcester <cdworces@ncsu.edu>
2024-04-21 15:54:41 -0400

    Add search text and input box

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      {/* Container for search text and input box */}
+      <div className="z-10 w-full max-w-5xl items-center font-mono text-sm lg:flex">
+        <div className="mr-4">Search:</div>
+        <input type="text" className="border border-gray-300 px-3 py-1 rounded-md" placeholder="finger" />
       </div>
     </main>
   );
