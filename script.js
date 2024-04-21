b4d95fdd3b195808743f234b594ceac89406ef3f
Chris Worcester <cdworces@ncsu.edu>
2024-04-21 11:24:45 -0400

    Clean up logs

--- script.js
+++ script.js
@@ -5,13 +5,7 @@ const wordCategories = {};
 // Wrap the code inside a DOMContentLoaded event listener to ensure it runs after the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
     buttonContainer = document.getElementById("buttonContainer");
-
     loadCSV("words.csv");
-
-    console.log("wordCategories:", wordCategories);
-
-    // Call the createButtons function initially
-    search("");
 });
 
 function createButtons(strings) {
@@ -24,34 +18,20 @@ function createButtons(strings) {
     // Clear existing buttons
     buttonContainer.innerHTML = "";
 
-    // If strings is provided and not empty, create buttons for the provided strings
-    if (strings && strings.length > 0) {
-        // Filter out duplicate strings
-        const uniqueStrings = [...new Set(strings)];
-
-        uniqueStrings.forEach(str => {
-            const button = document.createElement("button");
-            button.textContent = str;
-            buttonContainer.appendChild(button);
-        });
-    } else {
-        // // If no strings are provided, create buttons for all categories and words
-        // for (const category in wordCategories) {
-        //     // Create buttons for words in category
-        //     for (const word of wordCategories[category]) {
-        //         const wordButton = document.createElement("button");
-        //         wordButton.textContent = word;
-        //         buttonContainer.appendChild(wordButton);
-        //     }
-        // }
+    if (!strings || strings.length === 0) {
+        console.error("No strings provided to create buttons");
+        return;
     }
+
+    strings.forEach(str => {
+        const button = document.createElement("button");
+        button.textContent = str;
+        buttonContainer.appendChild(button);
+    });
 }
 
-// Filter the words based on the search query
-// 3 cases:
-// 1. if query is empty or null, display all buttons.
-// 2. if query is not empty and matches a category name or a word, display matching words.
-// 3. if query is not empty and does not match any category name or word, do not display any buttons.
+// Filter the words based on the search query and create a button for each word.
+// If the query is empty, display all words.
 function search(query) {
     if (query == null) {
         console.error("Query is null. You probably meant to call search with an empty string.");
@@ -59,17 +39,14 @@ function search(query) {
     }
 
     query = query.trim().toLowerCase();
-    console.log("Searching for: [" + query + "]")
     const results = [];
 
     // If the query is empty or null, display buttons for all words
     if (query === "") {
-        console.log("Empty query. Displaying all buttons");
-
         for (const category in wordCategories) {
-            console.log("Adding all words in category: " + category + " to results.");
+            // console.log("Adding all words in category: " + category + " to results.");
             for (const word of wordCategories[category]) {
-                console.log("\tAdding word: \"" + word + "\" to results.");
+                // console.log("\tAdding word: \"" + word + "\" to results.");
                 results.push(word);
             }
         }
@@ -101,6 +78,8 @@ function search(query) {
         }
     }
 
+    console.log("Searching for: \"" + query + "\". Results:");
+    console.log(results);
     createButtons(results);
 }
 
