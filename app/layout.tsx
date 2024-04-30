e9627ad7e071aa2ded9078b7af80a54cb113c111
Chris Worcester <cdworces@ncsu.edu>
2024-04-30 13:07:58 -0400

    Add description to title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Elden Scribe ",
+    title: "Elden Scribe | Write Elden Ring messages",
     description: "Write Elden Ring messages",
 };
 
