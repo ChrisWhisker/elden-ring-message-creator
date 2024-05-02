7e0a18bf5f29ef64e1566054b8a6bfcc0674e151
Chris Worcester <cdworces@ncsu.edu>
2024-05-02 12:43:48 -0400

    Replace links with buttons

--- app/message.tsx
+++ app/message.tsx
@@ -5,8 +5,8 @@ export default class Message {
     templates: Word[] = [];
     conjunction: Word | null = null;
     clauses: Word[] = []; // All words that aren't templates or conjunctions
-    wordLinks: JSX.Element[] = []; // Array of hyperlinks for each Word in the message
-    onUpdate: ((links: JSX.Element[]) => {}) | null = null; // Callback function for message update
+    wordButtons: JSX.Element[] = []; // Array of buttons for each Word in the message
+    onUpdate: ((buttons: JSX.Element[]) => {}) | null = null; // Callback function for message update
 
     // Singleton instance
     private static instance: Message | null = null;
@@ -25,15 +25,19 @@ export default class Message {
         return Message.instance;
     }
 
-    // Update the message string and hyperlinks array
+    // Update the message string and buttons array
     update(): void {
-        let hyperlinks: JSX.Element[] = []; // Temporary array to hold word hyperlinks
+        let buttons: JSX.Element[] = []; // Temporary array to hold word buttons
 
-        const addHyperlink = (word: Word | null, linkText: string) => {
-            hyperlinks.push(
-                <a href="#" onClick={() => this.handleClick(word)}>
-                    {linkText}
-                </a>
+        const addButton = (word: Word | null, buttonText: string) => {
+            buttons.push(
+                <button
+                    onClick={() => this.handleClick(word)}
+                    style={{ marginLeft: '2px', marginRight: '2px' }} // Adjust the margin as needed
+                    title={word ? word.word : undefined} // Conditionally set tooltip text
+                >
+                    <u>{buttonText}</u>
+                </button>
             );
         }
 
@@ -41,20 +45,20 @@ export default class Message {
             if (template != null) { // If template exists
                 if (clause != null) { // If clause exists
                     const clauseIndex = template.word.indexOf("****");
-                    addHyperlink(template, template.word.substring(0, clauseIndex));
+                    addButton(template, template.word.substring(0, clauseIndex));
                     if (clause != null) {
-                        addHyperlink(clause, clause.word);
+                        addButton(clause, clause.word);
                     }
-                    addHyperlink(template, template.word.substring(clauseIndex + 4));
+                    addButton(template, template.word.substring(clauseIndex + 4));
                 } else { // If clause does not exist
-                    addHyperlink(template, template.word + " ");
+                    addButton(template, template.word + " ");
                 }
             } else if (clause != null) { // If clause exists
                 // Add template without clause
-                addHyperlink(null, "[template] " + clause.word + " ");
+                addButton(null, "[template] " + clause.word + " ");
             } else {
                 // Add template without clause
-                addHyperlink(null, "[template] ");
+                addButton(null, "[template] ");
             }
         }
 
@@ -63,25 +67,33 @@ export default class Message {
 
         // Add the conjunction
         if (this.conjunction != null) { // Check if the conjunction exists
-            addHyperlink(this.conjunction, " " + this.conjunction.word + " ");
+            addButton(this.conjunction, " " + this.conjunction.word + " ");
         } else if (this.templates.length > 1 || this.clauses.length > 1) {
-            addHyperlink(null, " [conjunction] ");
+            addButton(null, " [conjunction] ");
+        }
+
+        // Add the second part of the message
+        if (this.templates.length > 1 || this.clauses.length > 1) {
             addTemplateAndClause(this.templates[1], this.clauses[1]);
         }
 
-        // Update the wordLinks array with the new hyperlinks
-        this.wordLinks = hyperlinks;
+        // Update the wordButtons array with the new buttons
+        this.wordButtons = buttons;
 
         // Call the onUpdate callback if it's set
         if (this.onUpdate) {
-            this.onUpdate(this.wordLinks);
+            this.onUpdate(this.wordButtons);
         }
     }
 
-    // Function to handle click events on hyperlinks
-    private handleClick(word: Word): void {
+    // Function to handle click events on buttons
+    private handleClick(word: Word | null): void {
+        if (word == null) {
+            console.log("Clicked on null");
+            return;
+        }
         console.log("Clicked on word:", word.word);
-        // You can perform any desired action here when a hyperlink is clicked
+        // You can perform any desired action here when a button is clicked
     }
 
     // Add a word to the message
