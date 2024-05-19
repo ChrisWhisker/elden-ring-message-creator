9ae412fc597df433d9dacb3fa8dbf37ce461c26a
Chris Worcester <cdworces@ncsu.edu>
2024-05-19 18:19:59 -0400

    Fix missing key error

--- app/buttons.tsx
+++ app/buttons.tsx
@@ -32,6 +32,7 @@ export class Button extends React.Component<{ word: Word }> {
         const word = this.props.word;
         return (
             <button
+                key = {word.toString()}
                 onClick={this.inMessage ? this.handleMessageRemove : this.handleMessageAdd}
                 title={`${word.category}: "${word.word}"`}
                 style={{
