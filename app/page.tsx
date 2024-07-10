72730972856ba9f81a84c7e7e6dc010f7ee47190
ChrisWhisker <chrisworc@gmail.com>
2024-07-10 12:20:57 -0400

    Clean up formatting

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,12 @@ const Home = () => {
               <path d="M12 .297C5.373.297 0 5.67 0 12.297c0 5.302 3.438 9.8 8.205 11.387.6.113.82-.26.82-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.334-1.758-1.334-1.758-1.09-.745.083-.729.083-.729 1.205.085 1.84 1.236 1.84 1.236 1.07 1.834 2.809 1.304 3.495.997.108-.774.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.93 0-1.31.468-2.38 1.235-3.22-.123-.305-.535-1.535.117-3.195 0 0 1.007-.322 3.3 1.23.957-.266 1.983-.399 3.005-.404 1.02.005 2.048.138 3.008.404 2.29-1.552 3.295-1.23 3.295-1.23.653 1.66.24 2.89.118 3.195.77.84 1.233 1.91 1.233 3.22 0 4.607-2.807 5.623-5.48 5.92.43.37.813 1.096.813 2.207v3.277c0 .32.218.694.825.577C20.565 22.092 24 17.597 24 12.297 24 5.67 18.627.297 12 .297z" />
             </svg>
           </a>
-          <a href="https://www.linkedin.com/in/chrisworcester/" target="_blank" rel="noopener noreferrer" className="mx-2">
+          <a
+            href="https://www.linkedin.com/in/chrisworcester/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2"
+          >
             <svg width="28" height="28" viewBox="0 0 24 24" fill="currentColor">
               <path d="M19.997 3H3.003C2.448 3 2 3.449 2 4.002v15.996C2 20.55 2.447 21 3.003 21h16.994c.555 0 1.003-.449 1.003-1.002V4.002C21 3.449 20.552 3 19.997 3zm-11.47 14.648h-2.91v-8.88h2.91v8.88zm-1.453-10.07c-.93 0-1.683-.753-1.683-1.683 0-.93.753-1.684 1.683-1.684.931 0 1.684.754 1.684 1.684 0 .93-.753 1.683-1.684 1.683zm11.062 10.07h-2.907v-4.855c0-1.157-.023-2.646-1.613-2.646-1.616 0-1.863 1.263-1.863 2.566v4.934h-2.906v-8.88h2.788v1.215h.039c.388-.735 1.337-1.51 2.753-1.51 2.942 0 3.483 1.936 3.483 4.455v5.72z" />
             </svg>
@@ -76,8 +81,19 @@ const Home = () => {
           <h2 className="text-lg body-text">Easily create messages for Elden Ring</h2>
         </div>
         <div className="h-20"></div>
-        <div id="Filter input" className="z-10 w-full max-w-5xl flex items-center justify-center body-text text-sm lg:text-base">
-          <input id="inputBox" ref={inputRef} type="text" className="inputBox w-full lg:w-96 px-4 py-2 rounded border border-gray-400 focus:border-yellow-400 focus:outline-none" value={searchText} onChange={handleInputChange} placeholder="Filter words" />
+        <div
+          id="Filter input"
+          className="z-10 w-full max-w-5xl flex items-center justify-center body-text text-sm lg:text-base"
+        >
+          <input
+            id="inputBox"
+            ref={inputRef}
+            type="text"
+            className="inputBox w-full lg:w-96 px-4 py-2 rounded border border-gray-400 focus:border-yellow-400 focus:outline-none"
+            value={searchText}
+            onChange={handleInputChange}
+            placeholder="Filter words"
+          />
         </div>
         <div className="h-4"></div>
         <div className="max-w-5xl h-full w-full flex flex-col overflow-hidden align-items-start">
