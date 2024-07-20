3fd048701b62d55c0a908a84f30ee245814614ce
ChrisWhisker <chrisworc@gmail.com>
2024-07-20 12:08:03 -0400

    Cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ const customMetadata = {
   description: "Generate custom messages for Elden Ring. Elden Scribe is an easy-to-use tool for creating Elden Ring messages.",
   keywords: "Elden Ring, message generator, custom messages, game tool, Elden Scribe",
   author: "Chris Worcester",
-  siteUrl: "https://chriswhisker.github.io/elden-ring-message-creator/",
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
@@ -25,18 +24,17 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         {/* Open Graph meta tags for Facebook */}
         <meta property="og:title" content={customMetadata.title} />
         <meta property="og:description" content={customMetadata.description} />
-        <meta property="og:url" content={customMetadata.siteUrl} />
         <meta property="og:type" content="website" />
         {/* Twitter card meta tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={customMetadata.title} />
         <meta name="twitter:description" content={customMetadata.description} />
         {/* Canonical URL */}
-        <link rel="canonical" href={customMetadata.siteUrl} />
         {/* Language */}
         <meta httpEquiv="Content-Language" content="en" />
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
+        <meta name="google-site-verification" content="LaNn_j2AC0c6w99SOPU92YGo2TBb2lYwkVyz75b7tf4" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
